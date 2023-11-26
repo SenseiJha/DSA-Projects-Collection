6858ed6e064a54e19dbd0f24bbeec2ddb7fd2b07
SenseiJha <149201562+SenseiJha@users.noreply.github.com>
2023-11-26 23:37:07 +0530

    Delete Elevator directory

--- Elevator/EmptyQueueException.java
+++
@@ -1,8 +0,0 @@
-package Elevator;
-
-@SuppressWarnings("serial")
-public class EmptyQueueException extends Exception {
-	public EmptyQueueException() {
-		System.out.println("Queue is Empty");
-	}
-}
--- Elevator/SimulationException.java
+++
@@ -1,8 +0,0 @@
-package Elevator;
-
-@SuppressWarnings("serial")
-public class SimulationException extends Exception {
-	public SimulationException() {
-		System.out.println("Simulation Exception Alert");
-	}
-}
